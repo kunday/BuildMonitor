7d565b225d8c5a34486e88545b5ea41aba58f174
Arvind Kunday <arvinda@thoughtworks.com>
2011-05-02 22:35:15 +0530

    Initial Commit

---
+++ BuildMonit/BuildMonitAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BuildMonitAppDelegate.h
+//  BuildMonit
+//
+//  Created by Arvind Kunday on 02/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BuildMonitAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ BuildMonit/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  BuildMonit
+//
+//  Created by Arvind Kunday on 02/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
